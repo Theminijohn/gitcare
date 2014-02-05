22182cc1982cc96592b418ecb63094d3c42b5a99
The Mini John <admin@phoenix-prime.gr>
2014-02-05 08:37:38 +0100

    Change Background / font-smoothing

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,6 +1,6 @@
 class Status < ActiveRecord::Base
 
   validates :title, :presence => true
-  validates :body, :presence => true
+  validates :body, :presence => true, :length => { :maximum => 600 }
 
 end
